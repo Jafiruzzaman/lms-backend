c2aa54a45729c030132b046608dc8be25e252c3e
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 15:35:05 +0600

    server connected with database with successfully

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { app } from "./app";
+import { config } from "./config/config";
+import { ConnectDB } from "./db/database";
+
+const startServer = async () => {
+  try {
+    ConnectDB()
+      .then(() => {
+        const port = config.port;
+        app.listen(port, () => {
+          console.log(
+            `server is running at ${port}. Server Connected with database`
+          );
+        });
+      })
+      .catch((err) => {
+        console.log(`something went wrong while listening server ${err}`);
+      });
+  } catch (error: any) {
+    console.log(`something went wrong while start server ${error.message}`);
+  }
+};
