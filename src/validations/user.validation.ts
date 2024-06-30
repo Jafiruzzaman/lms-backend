297b059e447515c8bb79f77ce42780cc1e18bff5
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 17:58:20 +0600

    add login validation

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -1,12 +1,7 @@
 import { z } from "zod";
 // const emailRegex = RegExp([/])
-export const signUpSchema = z.object({
-  name:z
-  .string({required_error:"user name is required"})
-  .trim()
-  .min(3,"user name must be at least 3 character")
-  .max(30,"user name must not be more than 30 character"),
-
+// loginSchema
+export const loginSchema = z.object({
   email:z
   .string({required_error:"user name is required"})
   .trim()
@@ -15,10 +10,17 @@ export const signUpSchema = z.object({
   // .regex()
   .min(3,"user name must be at least 3 character")
   .max(30,"user name must not be more than 30 character"),
-
   password:z
   .string({required_error:"password is required"})
   .trim()
   .min(8,"password must be at least 8 character")
   .max(80,"password must not be more than 80 character"),
+})
+// signUpSchema
+export const signUpSchema = loginSchema.extend({
+  name:z
+  .string({required_error:"user name is required"})
+  .trim()
+  .min(3,"user name must be at least 3 character")
+  .max(30,"user name must not be more than 30 character"),
 })
