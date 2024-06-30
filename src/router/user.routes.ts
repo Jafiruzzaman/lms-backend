c17bc133db6c821c9a1c4479dda467ab6ec783c5
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 16:46:23 +0600

    create auth routes

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -1,6 +1,14 @@
 import { Router } from "express";
+import { signUp } from "../auth/signup.auth";
+import { logout } from "../auth/logout.auth";
+import { login } from "../auth/login.auth";
+import { validate } from "../middlewares/validate";
+import { signUpSchema } from "../validations/user.validation";
 const userRoutes = Router();
-userRoutes.route("/users/auth/signup").post();
+// 
+userRoutes.route("/users/auth/signup").post(validate(signUpSchema),signUp);
+userRoutes.route("/users/auth/login").post(login);
+userRoutes.route("/users/auth/logout").post(logout);
 
 // export userRoutes
 export { userRoutes };
