194af0a6243ce77afb68b2ffcc9424a09daacf4e
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 18:32:33 +0600

    create course model

---
+++ src/model/course.model.ts
@@ -0,0 +1,4 @@
+import mongoose from "mongoose";
+import { Course } from "../interface/course.interface";
+import { courseSchema } from "../schema/course.schema";
+export default mongoose.model<Course>("Course",courseSchema)
