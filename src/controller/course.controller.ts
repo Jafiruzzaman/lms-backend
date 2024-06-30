71afa1931555871e1a6bbfbb6878a821fa09e86d
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 19:45:02 +0600

    fix some code

--- src/controller/course.controller.ts
+++ src/controller/course.controller.ts
@@ -2,6 +2,7 @@ import courseModel from "../model/course.model";
 import userModel from "../model/user.model";
 import { asyncHandler } from "../utils/asyncHandler";
 import { NextFunction, Request, Response } from "express";
+// ADD CLOUDINARY IN THIS FOUR CONTROLLER
 export const createCourse = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -39,7 +40,6 @@ export const createCourse = asyncHandler(
 );
 
 // get single course
-
 export const getCourse = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -77,7 +77,53 @@ export const getAllCourse = asyncHandler(
     }
   }
 );
-// delete course
+// update course
+export const updateCourse = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courseId = await req.params;
+      // @ts-ignore
+      const userId = req?.user;
+      const {
+        title,
+        description,
+        estimatedPrice,
+        discountPrice,
+        courseImage,
+        courseType,
+      } = req.body;
+      const author = await userModel.findById(userId).populate("name");
+      const getCourse = await courseModel.findById(courseId);
+      // TODO: Here use caching
+      if (getCourse?.author._id === userId) {
+        res.status(401).json({
+          message: "you can't update this course",
+        });
+        return next();
+      }
+      const updateCourse = await courseModel.findByIdAndUpdate(courseId, {
+        title: title,
+        description: description,
+        estimatedPrice: estimatedPrice,
+        discountPrice: discountPrice,
+        courseImage: courseImage,
+        courseType: courseType,
+        author: author,
+      });
+      return res.status(204).json({
+        message: "delete course successfully",
+        create_course: updateCourse,
+      });
+    } catch (error: any) {
+      return res.status(500).json({
+        message: "something went wrong while delete course",
+        error: error.message,
+      });
+    }
+  }
+);
+
+//delete course
 export const deleteCourse = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -89,6 +135,11 @@ export const deleteCourse = asyncHandler(
       let deleteCourse;
       if (getCourse?.author._id === userId) {
         deleteCourse = await courseModel.findByIdAndDelete(courseId);
+      } else {
+        res.status(401).json({
+          message: "you can't delete this course",
+        });
+        return next();
       }
       return res.status(204).json({
         message: "delete course successfully",
--- src/router/course.routes.ts
+++ src/router/course.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/authenticate";
-import { createCourse, deleteCourse, getAllCourse, getCourse } from "../controller/course.controller";
+import {
+  createCourse,
+  deleteCourse,
+  getAllCourse,
+  getCourse,
+  updateCourse,
+} from "../controller/course.controller";
 
 const courseRoutes = Router();
 // course CRUD
-courseRoutes.route("/create-course").post(authenticate,createCourse);
-courseRoutes.route("/get-course").get(authenticate,getCourse);
-courseRoutes.route("/get-all-course").get(authenticate,getAllCourse);
-courseRoutes.route("/update-course").put(authenticate);
-courseRoutes.route("/delete-course").delete(authenticate,deleteCourse);
+courseRoutes.route("/create-course").post(authenticate, createCourse);
+courseRoutes.route("/get-course").get(authenticate, getCourse);
+courseRoutes.route("/get-all-course").get(authenticate, getAllCourse);
+courseRoutes.route("/update-course").put(authenticate, updateCourse);
+courseRoutes.route("/delete-course").delete(authenticate, deleteCourse);
 // export
 export { courseRoutes };
