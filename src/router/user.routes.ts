6cb529744e154248065e087c5690166a387eaeb8
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 18:11:05 +0600

    add user profile

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -3,12 +3,15 @@ import { signUp } from "../auth/signup.auth";
 import { logout } from "../auth/logout.auth";
 import { login } from "../auth/login.auth";
 import { validate } from "../middlewares/validate";
-import { signUpSchema } from "../validations/user.validation";
+import { loginSchema, signUpSchema } from "../validations/user.validation";
+import { authenticate } from '../middlewares/authenticate';
+import { user } from "../auth/user.auth";
 const userRoutes = Router();
-// 
+// auth
 userRoutes.route("/users/auth/signup").post(validate(signUpSchema),signUp);
-userRoutes.route("/users/auth/login").post(login);
-userRoutes.route("/users/auth/logout").post(logout);
+userRoutes.route("/users/auth/login").post(validate(loginSchema),login);
+userRoutes.route("/users/auth/logout").post(authenticate,logout);
+userRoutes.route("/users/me").get(authenticate,user);
 
 // export userRoutes
 export { userRoutes };
