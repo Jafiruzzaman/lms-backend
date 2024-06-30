c59804eb02b61fb5e850f5d10b4ce3a91f907107
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 17:38:34 +0600

    add authenticate endpoint

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { config } from "../config/config";
+
+export const authenticate = async (req:Request,res:Response,next:NextFunction) => {
+  try {
+    const token = req.header('Authorization')?.replace("bearer ","") || req.cookies.accessToken
+    if (!token) {
+      res.status(401).json({
+        message:"Token required"
+      })
+      return next()
+    }
+    // decode the token
+    const decodedToken = await jwt.verify(token,config.access_token_secret)
+    if (!decodedToken) {
+      res.status(401).json({
+        message:"Invalid token"
+      })
+      return next()
+    }
+    // FIXME: remove it after seen the log
+    console.log('decoded token',decodedToken)
+    // @ts-ignore
+    req?.user = decodedToken._id
+    return next()
+  } catch (error) {
+    res.status(500).json({
+      message:"something went wrong while validating tokens",
+      error:error
+    })
+  }
+}
