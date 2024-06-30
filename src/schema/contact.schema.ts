a6a352ab96cc4f742ba8af0a28aa08f40435559e
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-30 19:59:01 +0600

    create contact schema

---
+++ src/schema/contact.schema.ts
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+import { Contact } from "../interface/contact.interface";
+export const contactSchema = new Schema<Contact>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
