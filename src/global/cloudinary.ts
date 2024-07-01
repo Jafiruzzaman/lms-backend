09862ae7861637685fa6f6a71fbbb137c1c00aed
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-07-01 15:45:51 +0600

    add file uploading functionality

---
+++ src/global/cloudinary.ts
@@ -0,0 +1,30 @@
+import { v2 } from "cloudinary";
+import { config } from "../config/config";
+import fs from "fs";
+v2.config({
+  cloud_name:config.cloudinary_cloud_name,
+  api_key:config.cloudinary_cloud_api_key,
+  api_secret:config.cloudinary_cloud_api_secret
+})
+export const fileUploadOnCloudinary = async (localFilePath:string,folder:string) => {
+  try {
+    // check if localFilePath exist or not
+    if (!localFilePath) {
+      throw new Error("local file path is missing")
+    }
+    // upload the file on cloudinary
+    const uploadResult = await v2.uploader.upload(localFilePath,{
+      folder:folder,
+      resource_type:"auto",
+    })
+    // remove the local file
+    fs.unlinkSync(localFilePath)
+    console.log('remove file')
+  } catch (error) {
+    if (!localFilePath) {
+      console.log("error",error)
+      throw new Error("something went wrong while uploading file on cloudinary Error",)
+      // return null
+    }
+  }
+}
